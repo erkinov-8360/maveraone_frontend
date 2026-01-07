db58a05a6f365c9b60fae4915d7f2c14bc5406ec
Ibrohim <ibrokhim.info@gmail.com>
2026-01-07 23:52:51 +0500

    wip

--- src/app/[locale]/test/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <div className="p-10">
-      <h1 className="text-4xl font-bold">Test Page Works!</h1>
-      <p>If you see this, the locale routing is working.</p>
-    </div>
-  );
-}
